4915c23d68962f6980a62e2f8944c66ed158a986
jljorro <jljorro@ucm.es>
2019-05-22 11:44:02 +0200

    Inicialización del proyecto

---
+++ src/main/java/es/ucm/fdi/gaia/RecoLibry_Examples/App.java
@@ -0,0 +1,13 @@
+package es.ucm.fdi.gaia.RecoLibry_Examples;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
